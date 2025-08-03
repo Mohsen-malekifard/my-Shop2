aa4f13dea074b299ae3a9f679bc2ffcca8338ac9
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-03 22:58:22 +0330

    M

--- my-app/src/app/lib/data.js
+++ my-app/src/app/lib/data.js
@@ -3,7 +3,7 @@ const data = [
     id: "1",
     name: "کفش اسپرت",
     price: 320000,
-    image: "https://via.placeholder.com/300x200",
+    image: "https://i.postimg.cc/5ymKxk4v/istockphoto-1445614323-1024x1024.jpg",
     description: "کفش سبک و راحت برای استفاده روزمره",
   },
   {
